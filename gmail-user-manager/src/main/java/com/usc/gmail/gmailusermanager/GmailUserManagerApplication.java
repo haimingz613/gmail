06b2a06ce0f0c16ee999847dbf7f0c02c59bda6f
haimingz613 <haimingz@usc.edu>
2020-09-02 17:29:56 -0700

    新加入

---
+++ gmail-user-manager/src/main/java/com/usc/gmail/gmailusermanager/GmailUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.usc.gmail.gmailusermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailUserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailUserManagerApplication.class, args);
+    }
+
+}
